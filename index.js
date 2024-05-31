786c13eeac041e632e31bce022453e47dae66dea
Julio Sanabria <juliosanabriazelaya@gmail.com>
2024-05-31 00:53:11 -0400

    Agg

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+app.use('/auth', require('./app/routes/auth'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
+
+
