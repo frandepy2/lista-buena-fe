97017c22263d6bc8ea816c328f782babb47f48e9
Francisco Alejandro Sanabria Zelaya <64796403+frandepy2@users.noreply.github.com>
2024-06-17 21:12:24 -0400

    configuraciones de heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieParser()); // Usar el middleware de cookies
 app.get('/',(req,res)=>{
     res.send('hola mundo');
 });
-app.use('/auth', require('./app/routes/auth'));
+app.use('/auth', require('./app/routes/auth.js'));
 app.use('/students', require('./app/routes/studentsRegister.js'));
 app.use('/teachers', require('./app/routes/teachersRegister.js'));
 
