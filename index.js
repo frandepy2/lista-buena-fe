94d4c43568b55d11a4c390ecb8a6e8c250c52165
Francisco Alejandro Sanabria Zelaya <64796403+frandepy2@users.noreply.github.com>
2024-06-18 09:25:59 -0400

    vuelve a agregar cors

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 const express = require('express');
 const cookieParser = require('cookie-parser'); // Importar el middleware de cookies
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cookieParser()); // Usar el middleware de cookies
+app.use(cors(
+    {  origin: '*', // Permitir cualquier dominio
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Permitir estos métodos
+        allowedHeaders: ['Content-Type', 'Authorization'], // Permitir estos encabezados
+    }
+));
 
 app.get('/',(req,res)=>{
     res.send('hola mundo');
